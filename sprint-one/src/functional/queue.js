a6b58fb381534de57473416b0e9fc1d22a6b171c
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 14:58:52 -0700

    attempting to modify code for dequeue in functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,10 +8,22 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     //add the value to queue
+    storage[count] = value;
     //increment count
+    count++;
   };
 
   someInstance.dequeue = function() {
+    //if current count is > 0
+    if (count > 0) {
+      //decrement count
+      count--;
+    }
+    //create a variable & set it to storage[0]
+    var current = storage[0];
+    //remove first value in storage
+    delete storage[0];
+    //
   };
 
   someInstance.size = function() {
