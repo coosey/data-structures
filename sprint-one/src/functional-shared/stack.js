25e98d1df57a398388c020ad08614ee98abbda80
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 16:27:39 -0700

    modifying queueMethods functions to return correct values

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,24 +1,29 @@
 var Stack = function() {
   var instances = {};
   //create a count & container in instances
-
+  instances.count = 0;
+  instances.container = {};
   //use extend()
-
+  _.extend(instances, stackMethods);
   //return instances
-
+  return instances;
 };
 //create push, pop, & size methods in stackMethods
 var stackMethods = {
   push: function(value) {
-
+    this.container[this.count] = this.value;
+    this.count++;
   },
 
   pop: function() {
-
+    if (this.count > 0) {
+      this.count--;
+    }
+    return this.container[this.count];
   },
 
   size: function() {
-
+    return this.count;
   }
 };
 
