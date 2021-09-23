1d0e32226aa8710a5b4e09070be77ddf7880361f
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 22:16:39 -0700

    Remodified Stack.prototype.method to correct order

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,16 +3,20 @@ var Stack = function() {
   this.container = {};
 };
 
-Stack.push.prototype = function(value) {
-
+Stack.prototype.push = function(value) {
+  this.container[this.count] = value;
+  this.count++;
 };
 
-Stack.pop.prototype = function() {
-
+Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    this.count--;
+  }
+  return this.container[this.count];
 };
 
-Stack.size.prototype = function() {
-
+Stack.prototype.size = function() {
+  return this.count;
 };
 
 
