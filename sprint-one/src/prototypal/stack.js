32d327da117255cd305b623f386734abfe52bf48
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 18:11:59 -0700

    creating a layout of push(value), pop(), & size() for stackMethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,9 +1,25 @@
 var Stack = function() {
   var instances = Object.create(stackMethods);
 
+  instances.container = {};
+  instances.count = 0;
+
   return instances;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+
+  },
+
+  pop: function() {
+
+  },
+
+  size: function() {
+
+  }
+};
 
 
