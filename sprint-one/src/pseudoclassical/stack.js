cd3126320ff95972713449b413e1ba8fdc6fbf51
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 21:53:48 -0700

    Implemented layout for Stack in Pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,18 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.container = {};
+};
+
+Stack.push.prototype = function(value) {
+
+};
+
+Stack.pop.prototype = function() {
+
+};
+
+Stack.size.prototype = function() {
+
 };
 
 
