fd54a2d09197a6571b8245fbb24992286a300095
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 21:44:39 -0700

    Implemented code into Queue.prototypes for enqueue, dequeue, & size

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -5,15 +5,21 @@ var Queue = function() {
 };
 
 Queue.prototype.enqueue = function(value) {
-
+  this.container[this.count] = value;
+  this.count++;
 };
 
 Queue.prototype.dequeue = function() {
-
+  var toBeRemoved = this.container[this.count];
+  if (this.count > this.newCount) {
+    delete this.container[this.count];
+    this.newCount++;
+    return toBeRemoved;
+  }
 };
 
 Queue.prototype.size = function() {
-
+  return Object.values(this.container).length;
 };
 
 
