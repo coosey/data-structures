9fea50e352eb5d0d7685d15de55e1e941699bb42
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-22 22:47:19 -0700

    implemented layout for queue in ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,19 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.container = {};
+    this.count = 0;
+    this.newCount = 0;
+  }
+  enqueue(value) {
+
+  }
+
+  dequeue() {
+
+  }
+
+  size() {
+
   }
 
 }
